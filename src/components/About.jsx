5b23873db6aba557fbe697d7e3d8aa06786da0a8
Kusum Bs <kusumbs24@gmail.com>
2024-10-14 22:33:56 +0200

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about">
         <h2 className="my-10 text-center text-3xl lg:text-8xl"> About Me</h2>
-        <div className="mx-4 mb-20 flex items-center justify-center">
+        <div className="mx-auto max-w-6xl mb-20 flex items-center justify-center">
             <p className="m-8 max-w-6xl text-2xl text-justify lg:text-xl">{ABOUT}</p>
         </div>
     </section>
