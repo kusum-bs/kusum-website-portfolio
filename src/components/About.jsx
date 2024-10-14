d3cc140288911ce68ff34ff62f1f79e584193af7
Kusum Bs <kusumbs24@gmail.com>
2024-10-14 21:08:51 +0200

    commit change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about">
         <h2 className="my-10 text-center text-3xl lg:text-8xl"> About Me</h2>
         <div className="flex items-center justify-center">
-            <p className="m-8 max-w-6xl text-2xl lg:text-6xl">{ABOUT}</p>
+            <p className="m-8 max-w-6xl text-2xl lg:text-xl">{ABOUT}</p>
         </div>
     </section>
   );
